35426345e73d68ed70a7884c60b9940cde3a172e
lovebird <lovebirdsx@gmail.com>
2022-01-28 09:32:37 +0800

    type test

--- promise/type.js
+++ promise/type.js
@@ -14,3 +14,11 @@ let p2 = p1.then()
 p2.then(() => timeout(500)).then(() => console.log('finished'))
 
 console.log('p1 == p2', p1 == p2)
+console.log('typeof p1', typeof p1)
+console.log('typeof p2', typeof p2)
+
+console.log(typeof undefined)
+console.log(typeof typeof undefined)
+console.log(typeof null)
+console.log(typeof a)
+console.log(typeof a === 'undefined')
