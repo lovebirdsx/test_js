e68e0fab593a186ad9e0b3586a99152360421fdb
lovebird <lovebirdsx@gmail.com>
2022-03-02 16:05:11 +0800

    context test

---
+++ typescript/misc/context.ts
@@ -0,0 +1,17 @@
+type Context<T> = {
+    value: T,
+}
+
+interface Foo {
+    name: string;
+    id: number;
+}
+
+function createContext<T>(t: T): Context<T> {
+return {
+    value: t,
+};
+}
+
+const fooContext = createContext<Foo>(undefined as any);
+fooContext.value = { name: 'lovebird', id: 100 };
