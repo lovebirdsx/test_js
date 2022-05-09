5ed887c79d8d4317527ca1208a95c2eaba694f12
lovebird <lovebirdsx@gmail.com>
2022-05-09 11:44:06 +0800

    up

--- javascript/base/undefined.js
+++ javascript/base/undefined.js
@@ -1,10 +1,17 @@
 let a;
-console.log(a == undefined)
-a = 5
-console.log(a == undefined)
+console.log(a == undefined);
+a = 5;
+console.log(a == undefined);
 
-let message
-if (message) {
-    console.log("message is defined")
-}
+let message;
+console.log('message is ', message);
 console.log(message) // 报错
+
+a = {
+    foo: undefined,
+    bar: 'hello',
+};
+
+console.log(a);
+console.log(JSON.stringify(a))
+console.log(JSON.parse(JSON.stringify(a)))
