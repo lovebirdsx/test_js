3d6e643a55d50d4589ae770b623771311e4f7ea3
lovebird <lovebirdsx@gmail.com>
2022-05-17 19:02:28 +0800

    up

---
+++ javascript/base/parse.js
@@ -0,0 +1,5 @@
+console.log("parseFloat('123')", parseFloat('123'));
+console.log("parseFloat('123abc')", parseFloat('123abc'));
+console.log("parseFloat('abc123a')", parseFloat('abc123a'));
+console.log("isNaN('123a')", isNaN('123a'));
+console.log("isNaN('123')", isNaN('123'));
