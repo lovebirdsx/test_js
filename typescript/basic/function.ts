67ccb7ccb5b5639c2f8f6edfeaa0219ea33c81e4
lovebird <lovebirdsx@gmail.com>
2022-07-03 00:17:36 +0800

    up

--- typescript/basic/function.ts
+++ typescript/basic/function.ts
@@ -120,6 +120,7 @@ class TestFunction {
         const d1 = makeDate(12345678);
         const d2 = makeDate(5, 5, 5);
         // const d3 = makeDate(1, 3);
+        console.log(d1, d2);
     }
 
     static Run(): void {
