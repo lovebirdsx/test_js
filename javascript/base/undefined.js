78e258b92c620ff847e6e2715a6d111a51079017
lovebird <lovebirdsx@gmail.com>
2022-08-08 00:46:27 +0800

    up

--- javascript/base/undefined.js
+++ javascript/base/undefined.js
@@ -15,3 +15,9 @@ a = {
 console.log(a);
 console.log(JSON.stringify(a))
 console.log(JSON.parse(JSON.stringify(a)))
+
+// 序列化之后会将undefined转化成null
+const a1 = ['a', undefined];
+console.log(a1);
+console.log(JSON.stringify(a1));
+console.log(JSON.parse(JSON.stringify(a1)));
