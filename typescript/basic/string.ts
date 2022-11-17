07873aec0124928bfb2a4d12145c726b87a76b8f
lovebird <lovebirdsx@gmail.com>
2022-11-17 21:05:24 +0800

    up

--- typescript/basic/string.ts
+++ typescript/basic/string.ts
@@ -11,3 +11,25 @@ function normalPath(path: string): string {
 }
 
 console.log(normalPath(path));
+
+function getNextName(name: string): string {
+    if (!name) {
+        return 'user-1';
+    }
+
+    const dashIndex = name.lastIndexOf('-');
+    if (dashIndex < 0) {
+        return `${name}-1`;
+    }
+
+    const id = Number.parseInt(name.substring(dashIndex + 1));
+    if (isNaN(id)) {
+        return `${name}-1`;
+    }
+
+    return `${name.substring(0, dashIndex)}-${id + 1}`;
+}
+
+console.log(getNextName('hello'));
+console.log(getNextName('hello-1'));
+console.log(getNextName('hello-fuck'));
