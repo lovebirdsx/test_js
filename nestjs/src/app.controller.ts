2bd298301033348e83dd0c2dcefa97ee8a625a72
lovebird <lovebirdsx@gmail.com>
2023-10-10 09:18:40 +0800

    加入nest计算服务

--- nestjs/src/app.controller.ts
+++ nestjs/src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Query } from '@nestjs/common';
+import { AppService, CalcService } from './app.service';
 
 @Controller()
 export class AppController {
@@ -8,5 +8,38 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+}
+
+@Controller('calc')
+export class CalcController {
+  constructor(private readonly calcService: CalcService) {}
+
+  @Get('add')
+  add(@Query('a') a: string, @Query('b') b: string): number {
+    const na = parseFloat(a);
+    const nb = parseFloat(b);
+    return this.calcService.add(na, nb);
+  }
+
+  @Get('sub')
+  sub(@Query('a') a: string, @Query('b') b: string): number {
+    const na = parseFloat(a);
+    const nb = parseFloat(b);
+    return this.calcService.sub(na, nb);
+  }
+
+  @Get('mul')
+  mul(@Query('a') a: string, @Query('b') b: string): number {
+    const na = parseFloat(a);
+    const nb = parseFloat(b);
+    return this.calcService.mul(na, nb);
+  }
+
+  @Get('div')
+  div(@Query('a') a: string, @Query('b') b: string): number {
+    const na = parseFloat(a);
+    const nb = parseFloat(b);
+    return this.calcService.div(na, nb);
   }
 }
--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController, CalcController } from './app.controller';
+import { AppService, CalcService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, CalcController],
+  providers: [AppService, CalcService],
 })
 export class AppModule {}
