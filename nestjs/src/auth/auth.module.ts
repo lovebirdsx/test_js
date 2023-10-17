06bb08b0c56bb4a0d361725dd067e087cc5fd552
lovebird <lovebirdsx@gmail.com>
2023-10-17 09:10:20 +0800

    up

--- nestjs/src/auth/auth.module.ts
+++ nestjs/src/auth/auth.module.ts
@@ -7,6 +7,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './const';
 import { JwtStrategy } from './jwt.strategy';
 
+// 参考：https://docs.nestjs.cn/10/security?id=%e8%ae%a4%e8%af%81%ef%bc%88authentication%ef%bc%89
 @Module({
   imports: [
     UsersModule,
