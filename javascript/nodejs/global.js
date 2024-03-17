700d1931e4c286c67e6582ebe2165e61f8a72783
lovebird <lovebirdsx@gmail.com>
2024-03-17 22:29:36 +0800

    输出全局对象

---
+++ javascript/nodejs/global.js
@@ -0,0 +1,10 @@
+// 输出nodejs全局对象
+
+console.log(global);
+console.log(process);
+console.log(Buffer);
+console.log(__dirname);
+console.log(__filename);
+console.log(module);
+console.log(exports);
+console.log(require);
