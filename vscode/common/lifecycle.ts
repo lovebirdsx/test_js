843f4321134de5ced9686be9c0ed0c1346853bd4
lovebird <lovebirdsx@gmail.com>
2024-07-24 19:45:30 +0800

    加入ipc.net

--- vscode/common/lifecycle.ts
+++ vscode/common/lifecycle.ts
@@ -2,26 +2,26 @@ import { once } from './functional';
 import { Iterable } from './iterator';
 
 export interface IDisposableTracker {
-	/**
-	 * Is called on construction of a disposable.
-	*/
-	trackDisposable(disposable: IDisposable): void;
+    /**
+     * Is called on construction of a disposable.
+    */
+    trackDisposable(disposable: IDisposable): void;
 
-	/**
-	 * Is called when a disposable is registered as child of another disposable (e.g. {@link DisposableStore}).
-	 * If parent is `null`, the disposable is removed from its former parent.
-	*/
-	setParent(child: IDisposable, parent: IDisposable | null): void;
+    /**
+     * Is called when a disposable is registered as child of another disposable (e.g. {@link DisposableStore}).
+     * If parent is `null`, the disposable is removed from its former parent.
+    */
+    setParent(child: IDisposable, parent: IDisposable | null): void;
 
-	/**
-	 * Is called after a disposable is disposed.
-	*/
-	markAsDisposed(disposable: IDisposable): void;
+    /**
+     * Is called after a disposable is disposed.
+    */
+    markAsDisposed(disposable: IDisposable): void;
 
-	/**
-	 * Indicates that the given object is a singleton which does not need to be disposed.
-	*/
-	markAsSingleton(disposable: IDisposable): void;
+    /**
+     * Indicates that the given object is a singleton which does not need to be disposed.
+    */
+    markAsSingleton(disposable: IDisposable): void;
 }
 
 /**
@@ -34,8 +34,52 @@ export interface IDisposableTracker {
 const TRACK_DISPOSABLES = false;
 let disposableTracker: IDisposableTracker | null = null;
 
+if (TRACK_DISPOSABLES) {
+    const __is_disposable_tracked__ = '__is_disposable_tracked__';
+    setDisposableTracker(new class implements IDisposableTracker {
+        trackDisposable(x: IDisposable): void {
+            const stack = new Error('Potentially leaked disposable').stack!;
+            setTimeout(() => {
+                if (!(x as any)[__is_disposable_tracked__]) {
+                    console.log(stack);
+                }
+            }, 3000);
+        }
+
+        setParent(child: IDisposable, parent: IDisposable | null): void {
+            if (child && child !== Disposable.None) {
+                try {
+                    (child as any)[__is_disposable_tracked__] = true;
+                } catch {
+                    // noop
+                }
+            }
+        }
+
+        markAsDisposed(disposable: IDisposable): void {
+            if (disposable && disposable !== Disposable.None) {
+                try {
+                    (disposable as any)[__is_disposable_tracked__] = true;
+                } catch {
+                    // noop
+                }
+            }
+        }
+        markAsSingleton(disposable: IDisposable): void { }
+    });
+}
+
 export function setDisposableTracker(tracker: IDisposableTracker | null): void {
-	disposableTracker = tracker;
+    disposableTracker = tracker;
+}
+
+export function trackDisposable<T extends IDisposable>(x: T): T {
+    disposableTracker?.trackDisposable(x);
+    return x;
+}
+
+export function markAsDisposed(disposable: IDisposable): void {
+    disposableTracker?.markAsDisposed(disposable);
 }
 
 export interface IDisposable {
@@ -122,12 +166,12 @@ export class DisposableStore implements IDisposable {
 }
 
 function setParentOfDisposables(children: IDisposable[], parent: IDisposable | null): void {
-	if (!disposableTracker) {
-		return;
-	}
-	for (const child of children) {
-		disposableTracker.setParent(child, parent);
-	}
+    if (!disposableTracker) {
+        return;
+    }
+    for (const child of children) {
+        disposableTracker.setParent(child, parent);
+    }
 }
 
 
@@ -135,9 +179,9 @@ function setParentOfDisposables(children: IDisposable[], parent: IDisposable | n
  * Combine multiple disposable values into a single {@link IDisposable}.
  */
 export function combinedDisposable(...disposables: IDisposable[]): IDisposable {
-	const parent = toDisposable(() => dispose(disposables));
-	setParentOfDisposables(disposables, parent);
-	return parent;
+    const parent = toDisposable(() => dispose(disposables));
+    setParentOfDisposables(disposables, parent);
+    return parent;
 }
 
 export function toDisposable(fn: () => void): IDisposable {
@@ -147,13 +191,24 @@ export function toDisposable(fn: () => void): IDisposable {
 export abstract class Disposable implements IDisposable {
     static readonly None = Object.freeze<IDisposable>({ dispose() { } });
 
-    private readonly _store = new DisposableStore();
+    protected readonly _store = new DisposableStore();
 
     constructor() {
-        this._store.add(this);
+        trackDisposable(this);
+        setParentOfDisposables([this._store], this);
     }
 
     dispose(): void {
         this._store.dispose();
+    }
+
+    /**
+     * Adds `o` to the collection of disposables managed by this object.
+     */
+    protected _register<T extends IDisposable>(o: T): T {
+        if ((o as unknown as Disposable) === this) {
+            throw new Error('Cannot register a disposable on itself!');
+        }
+        return this._store.add(o);
     }
 }
