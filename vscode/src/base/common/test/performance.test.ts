4fc167dabc00aea540be581f38a2659b1da26dea
lovebird <lovebirdsx@gmail.com>
2024-07-25 09:01:09 +0800

    修正测试错误

--- vscode/src/base/common/test/performance.test.ts
+++ vscode/src/base/common/test/performance.test.ts
@@ -2,18 +2,24 @@ import { getMarks, mark } from "../performance";
 import * as assert from 'assert';
 
 suite('Performance', () => {
+    setup(() => {
+        performance.clearMarks();
+    });
+
     test('simple', () => {
-        mark('start');
-        mark('end');
-        const marks = getMarks();
-        assert.strictEqual(marks.length, 3);
-        assert.strictEqual(marks[1].name, 'start');
-        assert.strictEqual(marks[2].name, 'end');
+        performance.mark('start');
+        performance.mark('end');
+
+        const marks = performance.getEntriesByType('mark');
+        assert.strictEqual(marks.length, 2);
+        assert.strictEqual(marks[0].name, 'start');
+        assert.strictEqual(marks[1].name, 'end');
     });
 
     test('nodejs perf', () => {
         performance.mark('start');
         performance.mark('end');
+
         const marks = performance.getEntries();
         assert.strictEqual(marks.length, 2);
     });
