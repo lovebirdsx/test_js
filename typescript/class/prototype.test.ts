5952b8432434482ee178bdbbe627975012abb191
lovebird <lovebirdsx@gmail.com>
2024-08-24 10:42:15 +0800

    up

--- typescript/class/prototype.test.ts
+++ typescript/class/prototype.test.ts
@@ -10,4 +10,30 @@ describe('prototype', () => {
     expect(cat.meow()).toEqual('meow');
     expect(cat.meow).toEqual(Cat.prototype.meow);
   });
+
+  it('static field', () => {
+    class Cat {
+      static category = 'animal';
+    }
+
+    const properties = Object.getOwnPropertyDescriptors(Cat);
+    expect(properties.category.value).toEqual('animal');
+  });
+
+  it('instance field', () => {
+    class Cat {
+      name = 'Tom';
+
+      age?: number;
+    }
+
+    const cat = new Cat();
+    const properties = Object.getOwnPropertyDescriptors(cat);
+    const keys = Object.keys(properties);
+
+    expect(keys).toEqual(['name', 'age']);
+    expect(properties.name.value).toEqual('Tom');
+    expect(properties.age).toBeDefined();
+    expect(properties.age?.value).toBeUndefined();
+  });
 });
